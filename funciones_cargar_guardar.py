bee7a256b2dd8c436a42446a1ca28f26064e658f
dimaptfd <sissormarusebastomoe7@gmail.com>
2024-06-03 16:55:22 -0500

    holaaa

--- funciones_cargar_guardar.py
+++ funciones_cargar_guardar.py
@@ -1,38 +1,38 @@
 import json
+nombre_archivo = 'usuarios_delta.json'
 
-def cargar_y_guardar(nuevos_datos, nombre_archivo='usuarios_delta.json'):
-    try:
-        with open(nombre_archivo, 'r') as archivo:
-            datos = json.load(archivo)
+def guardar(nuevos_datos):
+    with open(nombre_archivo, "w") as archivo:
+        json.dump(nuevos_datos ,archivo, indent=4)
+
+
+
+def cargar():
+    try: 
+        with open(nombre_archivo, "r") as read:
+            datos = json.load(read)
+            return datos
     except FileNotFoundError:
-        datos = []
-        users = { } 
-        datos[0] = users
+        return{"ingresados": { }}
 
 
-    datos.append(nuevos_datos)
-
-    with open(nombre_archivo, 'w') as archivo:
-        json.dump(datos, archivo, indent=4)
-
-    return datos
 
     
-def buscar_usuario(docu_buscar, archivo='usuarios.json'):
-    try:
-        with open(archivo, 'r') as archivo:
-            datos = json.load(archivo)
-    except FileNotFoundError:
-        return None
-    for user in datos:
-        if user.get("docu") == docu_buscar:
-            return user
-    return None
-
-def cambiar_estado(user, nuevo_estado, archivo='usuarios.json'):
-    if user:
-        user["estado"] = nuevo_estado
-        with open(archivo, 'w') as archivo:
-            json.dump(datos, archivo, indent=4)
-        return True
-    return False
+# def buscar_usuario(docu_buscar, archivo='usuarios.json'):
+#    try:
+#        with open(archivo, 'r') as archivo:
+#            datos = json.load(archivo)
+#    except FileNotFoundError:
+#        return None
+#    for user in datos:
+#        if user.get("docu") == docu_buscar:
+#            return user
+#    return None
+#
+# def cambiar_estado(user, nuevo_estado, archivo='usuarios.json'):
+#    if user:
+#        user["estado"] = nuevo_estado
+#        with open(archivo, 'w') as archivo:
+#            json.dump(datos, archivo, indent=4)
+#        return True
+#    return False
--- funciones_registro.py
+++ funciones_registro.py
@@ -1,40 +1,40 @@
 
 
-from funciones_cargar_guardar import cargar_y_guardar
+import funciones_cargar_guardar as brr
 
 
 def pedir_datos():
-    
+    rhlm= brr.cargar()
     usuarios = {}
     print("**********************************************************************")
     print("para realizar la inscripcion, llene el siguiente formulario\n(tome en cuenta que su edad debe ser mayor o igual de 17 y menor que 30 años):  ")
     edad = int(input("edad: "))
+    docu = int(input("documento: "))
     if edad >= 18 and edad < 30:  
-        
         usuarios["nombres"] = str(input("nombres: ")).title()
         usuarios["apellidos"] = str(input("apellidos: ")).title()
-        docu = usuarios["documento"] = int(input("documento: "))
         usuarios["direccion"] = input("direccion: ")
         usuarios["num_tel"] = int(input("numero de celular / telefono: "))
         usuarios["estados"] ="en proceso de inscripcion" 
+        rhlm["ingresados"][docu] = usuarios
+        brr.guardar(rhlm)
         
-        cargar_y_guardar(usuarios)
+        
     elif edad == 17:
         
         usuarios["nombres"] = str(input("nombres: ")).title()
         usuarios["apellidos"] = str(input("apellidos: ")).title()
         usuarios["direccion"] = input("direccion: ")
         usuarios["num_tel"] = int(input("numero de celular / telefono: "))
-        docu = usuarios["documento"] = int(input("documento: "))
         print("al ser menor de edad, estamos obligados a pedir los datos de algun acudiente: ")
         usuarios["acudiente_nombre"] = str(input("acudiente; nombres y apellidos: ")).title()
         usuarios["acudiente documento"] = int(input("acudiente; documento: "))
         usuarios["parentesco acudiente"] = str(input("parentesco: "))
         usuarios["num_tel acudiente"] = int(input("numero / telefono, del acudiente: "))
         usuarios["estados"] ="en proceso de inscripcion"      
-  
+        rhlm["ingresados"][docu] = usuarios
         
-        cargar_y_guardar(usuarios)
+        brr.guardar(rhlm)
     else: 
         print("lo sentimos, estas fuera del rango de edad para la inscripcion")
-            
+pedir_datos()            
