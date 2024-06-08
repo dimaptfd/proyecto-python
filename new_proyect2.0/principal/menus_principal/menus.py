030660d5edd006bcf528fcd06aa8349aa3cd09f1
dimaptfd <sissormarusebastomoe7@gmail.com>
2024-06-08 14:33:36 -0500

    graves

---
+++ new_proyect2.0/principal/menus_principal/menus.py
@@ -0,0 +1,52 @@
+menu_principal =  (
+   "1 para ingresar como camper\n"
+   "2 para ingresar como trainer\n"
+   "3 para ingresar como cordinador\n"
+   "4 para registrarse"
+)
+menu_camper = (
+    "1 para consultar informacion usuario\n"
+    "2 para consultar notas\n"
+    "3 para consultar reportes\n"
+    "4 para consultar camperpoints"
+    "5 para volver..."
+)
+menu_trainer = (
+    "1 para ver/modificar informacion del trainer\n"
+    "2 para ver grupos/rutas/horarios/salones\n"
+    "4 para volver..."
+)
+menu_trainer_grupos = (
+    #mostrar solo los grupos a los que al docente se la a sido asiganado
+    #dar opcion de mostrar informacion de campers
+    #reportes
+    #dar opcion de calificar, mostrar los skills y7 dar a elegir
+    #mostrar solo los campers que no han sido calificados en el skills selecionado
+    #pedir numero de documento del camper
+    #pedir nota 
+    #voler...
+)
+menu_cordinador = (
+    "1 para ver grupos\n"
+    "2 Para calificar prueba de ingreso\n"
+    "3 para rutas/grupos/trainers"
+    "4 para volver..."
+    
+)
+menu_cordinador_grupos = (
+    "1 para agregar a camer a un ruta/grupo\n"
+    #mostrar todos los grupos
+    #opcion de ver informacion de un grupo en especifico
+    "0 para volver..."
+)
+menu_cordinador_rutas = (
+    #mostrar rutas
+    "1 para ver informacion de ruta"
+    "0 para volver..."
+)
+    
+
+
+
+ 
+
